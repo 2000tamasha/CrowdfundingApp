a212c77f5ce25709b0356b012ee9b4e4231b4bd3
Thamasha <thamasha.kodithuwakku@gmail.com>
2024-10-04 14:55:56 +0800

    body parser included & post method added

--- 24351177 A2-api/controllerAPI/donation-controller.js
+++ 24351177 A2-api/controllerAPI/donation-controller.js
@@ -6,4 +6,36 @@ var express = require('express');
 var router = express.Router();
 
 
+
+//post for inserting donations
+router.post("/", (req, res)=>{
+	var DONATION_ID = req.body.DONATION_ID;
+	var DATE = req.body.DATE;
+	var AMOUNT = req.body.AMOUNT;
+        var GIVER = req.body.GIVER;
+        var FUNDRAISER_ID = req.body.FUNDRAISER_ID;
+	connection.query("INSERT INTO DONATION VALUES("+DONATION_ID+",'"+DATE+"','"+AMOUNT+"','"+GIVER+"',"+FUNDRAISER_ID+")",  
+	(err, result)=> {
+		 if (err){
+			 console.error("Error while retrieve the data" + err);
+		 }else{
+			 res.send({insert:"success"});
+		 }
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
